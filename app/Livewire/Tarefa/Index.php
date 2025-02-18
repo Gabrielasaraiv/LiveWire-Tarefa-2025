a0bef1435e39135a5d030cd7e9590f94ebaeac6c
Gabrielasaraiv <gabrielasaraivaaraujo358@gmail.com>
2025-02-18 14:58:58 -0300

    modified:   app/Livewire/Tarefa/Index.php
    	modified:   resources/views/livewire/tarefa/index.blade.php

--- app/Livewire/Tarefa/Index.php
+++ app/Livewire/Tarefa/Index.php
@@ -7,10 +7,24 @@ use Livewire\Component;
 
 class Index extends Component
 {
+
+    public $nome;
+    public $data_hora;
+    public $descricao;
+
     public function render()
     {
         $tarefas = Tarefa::all();
         
         return view('livewire.tarefa.index', compact('tarefas')); // compact vai enviar a variavel para dentro da tela
+    }
+
+    public function abrirModalVisualizar($tarefaId){
+        $tarefa = Tarefa::find($tarefaId);
+        if($tarefa){
+            $this->nome = $tarefa->nome;
+            $this->data_hora = $tarefa->data_hora;
+            $this->descricao = $tarefa->descricao;
+        }
     }
 }
